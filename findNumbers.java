7ed390557e4339b216663ae3408b0802b413bdf6
josephggg5 <josephggg5@gmail.com>
2020-02-05 14:14:08 -0500

    Add files via upload
    findNumbers (LC 1295)

---
+++ findNumbers.java
@@ -0,0 +1,29 @@
+package Problem;
+
+public class findNumbers {
+	public static int findNumber(int[] nums) {
+		if(nums.length == 0) {
+			return -1;
+		}
+		
+		int digitCounter = 0;
+		int evenCounter = 0;
+		for(int i=0; i<nums.length;i++) {
+			while(nums[i]!=0) {
+				nums[i] /= 10;
+				digitCounter++;
+			}
+			if(digitCounter%2 == 0) {
+				evenCounter++;
+			}
+			digitCounter = 0;
+		}
+		return evenCounter;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = {12,345,2,6,7896};
+		System.out.println(findNumber(nums));
+	}
+
+}
